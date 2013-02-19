2dfe6d47e57cc690011efee999933cfa976fb8b4
gmatte <guillaume.matte@laposte.net>
2013-02-19 19:11:58 +0100

    JComboBox dans JTable
    La liste déroulante ne génère plus d'erreur dans la JTable. Elle marche
    à la perfection

--- FenetreFiche.java
+++ FenetreFiche.java
@@ -15,45 +15,47 @@ import javax.swing.DefaultCellEditor;
 import javax.swing.*;
 import javax.swing.border.BevelBorder;
 import javax.swing.border.Border;
+import javax.swing.table.AbstractTableModel;
 
 public class FenetreFiche extends JFrame implements ActionListener {
+	private boolean DEBUG = false;
+	
 	private java.sql.Date sDate;
 	private java.util.Date uDate;
-	
+
 	private JPanel panelGlobal;
 	private JPanel panelGlobalForfait;
 	private JPanel panelGlobalHorsForfait;
 	private JPanel panelGlobalHorsClassification;
 	private JPanel panelGlobalFicheFrais;
 	private JPanel panelBouton;
-	
+
 	// Variables pour les frais forfait
 	private JPanel panelLabelFraisForfait;
 	private JPanel panelTableFraisForfait;
 	private JLabel libelleTableForfait;
 	private JTable tableFraisForfait;
 	private JScrollPane scrollFraisForfait;
-	
+
 	// Variables pour les frais hors forfait
 	private JPanel panelLabelHorsForfait;
 	private JPanel panelTableHorsForfait;
 	private JLabel libelleTableHorsForfait;
 	private JTable tableHorsForfait;
 	private JScrollPane scrollHorsForfait;
-	
+
 	// Variables pour les hors classification
 	private JPanel panelLabelHorsClassification;
 	private JPanel panelTableHorsClassification;
 	private JLabel libelleHorsClassification;
 	private JTable tableHorsClassification;
 	private JScrollPane scrollHorsClassification;
-	
+
 	/* ------------------------------------------- */
-	
-	private JComboBox listeEtatFrais;
+
 	private JButton validerFiche;
 	private JButton retour;
-	
+
 	private String idVisiteur;
 	private String mois;
 	private String mmaaaa;
@@ -61,6 +63,7 @@ public class FenetreFiche extends JFrame implements ActionListener {
 	private String prenomComptable;
 	private String nomVisiteur;
 	private String prenomVisiteur;
+	
 
 	public FenetreFiche(String idVisiteur, String mois, String nomComptable, String prenomComptable, String nomVisiteur, String prenomVisiteur){
 		this.idVisiteur = idVisiteur;
@@ -70,7 +73,7 @@ public class FenetreFiche extends JFrame implements ActionListener {
 		this.nomVisiteur = nomVisiteur;
 		this.prenomVisiteur = prenomVisiteur;
 		this.mmaaaa = mois.substring(4)+"/"+mois.substring(0, 4);
-		
+
 		this.sDate = new java.sql.Date(System.currentTimeMillis());
 		this.uDate = new java.util.Date();
 		/*
@@ -78,14 +81,16 @@ public class FenetreFiche extends JFrame implements ActionListener {
 		 */
 		sDate = new java.sql.Date(uDate.getTime());
 		
+		
 		ArrayList<EtatFrais>lesEtatsFrais = Passerelle.chargerLesEtatsFrais();
-		listeEtatFrais = new JComboBox();
-		for(EtatFrais unEtatfrais : lesEtatsFrais){
-			String idEtat = unEtatfrais.getIdEtat();
-			String libelleEtat = unEtatfrais.getLibelleEtat();
-			listeEtatFrais.addItem(new EtatFrais(idEtat, libelleEtat));
+		
+		JComboBox listeEtatFrais = new JComboBox();
+		
+		for(EtatFrais unEtatFrais : lesEtatsFrais){
+			listeEtatFrais.addItem(unEtatFrais.getLibelleEtat());
 		}
 		
+
 		// Instanciation des panels globaux et du panel pour le bouton valider
 		panelGlobal = new JPanel();
 		panelGlobalForfait = new JPanel();
@@ -93,39 +98,39 @@ public class FenetreFiche extends JFrame implements ActionListener {
 		panelGlobalHorsClassification = new JPanel();
 		panelGlobalFicheFrais = new JPanel();
 		panelBouton = new JPanel();
-		
+
 		// Instanciation des panels et label des frais forfait
 		panelLabelFraisForfait = new JPanel();
 		panelLabelFraisForfait.setBackground(Color.orange);
 		panelLabelFraisForfait.setBorder(BorderFactory.createLineBorder(Color.black));
 		panelTableFraisForfait = new JPanel();
 		libelleTableForfait = new JLabel("Frais forfaitisés");
-		
+
 		// Instanciation des panels et labels des hors forfait
 		panelLabelHorsForfait = new JPanel();
 		panelLabelHorsForfait.setBackground(Color.orange);
 		panelLabelHorsForfait.setBorder(BorderFactory.createLineBorder(Color.black));
 		panelTableHorsForfait = new JPanel();
 		libelleTableHorsForfait = new JLabel("Frais Hors Forfait");
-		
+
 		// Instanciation des panels et label des hors classification
 		panelLabelHorsClassification = new JPanel();
 		panelLabelHorsClassification.setBackground(Color.orange);
 		panelLabelHorsClassification.setBorder(BorderFactory.createLineBorder(Color.black));
 		panelTableHorsClassification = new JPanel();
 		libelleHorsClassification = new JLabel("Hors classification");
-		
+
 		validerFiche = new JButton("Valider");
 		validerFiche.addActionListener(this);
 		retour = new JButton("Retour");
 		retour.addActionListener(this);
-		
+
 		this.setTitle("Fiche de frais du visiteur "+nomVisiteur+" "+prenomVisiteur+" pour la période "+mmaaaa);
 		this.setSize(900, 500);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setResizable(true);
 		this.setLocationRelativeTo(null);
-		
+
 		// Création du tableau des frais forfaitisés
 		ArrayList<FraisForfait>lesFraisForfait = Passerelle.chargerLesFichefraisForfait(idVisiteur, mois);
 		int tailleForfait = lesFraisForfait.size();
@@ -145,7 +150,7 @@ public class FenetreFiche extends JFrame implements ActionListener {
 		tableFraisForfait.getTableHeader().setBackground(new Color(91, 111, 192));
 		tableFraisForfait.getTableHeader().setForeground(Color.white);
 		scrollFraisForfait = new JScrollPane(tableFraisForfait);
-		
+
 		// Création du tableau des frais hors forfait
 		ArrayList<FraisHorsForfait>lesHorsForfait = Passerelle.chargerLesFicheFraisHorsForfait(idVisiteur, mois);
 		int tailleHorsForfait = lesHorsForfait.size();
@@ -162,15 +167,16 @@ public class FenetreFiche extends JFrame implements ActionListener {
 			dataHorsForfait[indexHorsForfait][2] = unMontant+"";
 			dataHorsForfait[indexHorsForfait][3] = libelleEtat;
 			//dataHorsForfait[indexHorsForfait][4] = listeEtatFrais+"";
+
 			
 		}
 		tableHorsForfait = new JTable(dataHorsForfait, titleHorsForfait);
 		tableHorsForfait.getColumnModel().getColumn(4).setCellEditor(new DefaultCellEditor(listeEtatFrais));
 		tableHorsForfait.getTableHeader().setBackground(new Color(91, 111, 192));
 		tableHorsForfait.getTableHeader().setForeground(Color.white);
-		
+
 		scrollHorsForfait = new JScrollPane(tableHorsForfait);
-		
+
 		// Création du tableau des frais hors classification
 		ArrayList<HorsClassification>lesHorsClassification = Passerelle.chargerLesFichesHorsClassification(idVisiteur, mois);
 		int tailleHorsClassification = lesHorsClassification.size();
@@ -189,64 +195,65 @@ public class FenetreFiche extends JFrame implements ActionListener {
 		tableHorsClassification.getTableHeader().setBackground(new Color(91, 111, 192));
 		tableHorsClassification.getTableHeader().setForeground(Color.white);
 		scrollHorsClassification = new JScrollPane(tableHorsClassification);
-		
-		// Instanciation de l'affichage des frais forfait		
+
+		// Instanciation de l'affichage des frais forfait
 		panelLabelFraisForfait.setLayout(new FlowLayout());
 		panelLabelFraisForfait.add(libelleTableForfait);
-		
+
 		panelTableFraisForfait.setLayout(new BorderLayout());
 		panelTableFraisForfait.add(tableFraisForfait.getTableHeader(), BorderLayout.NORTH);
 		panelTableFraisForfait.add(tableFraisForfait, BorderLayout.CENTER);
-		
+
 		panelGlobalForfait.setLayout(new BorderLayout());
 		panelGlobalForfait.add(panelLabelFraisForfait, BorderLayout.NORTH);
 		panelGlobalForfait.add(panelTableFraisForfait, BorderLayout.CENTER);
 		panelGlobalForfait.add(new JLabel(lesFraisForfait.get(0).getLibelleEtat()), BorderLayout.SOUTH);
-		
+
 		// Instanciation de l'affichage des frais forfait
 		panelLabelHorsForfait.setLayout(new FlowLayout());
 		panelLabelHorsForfait.add(libelleTableHorsForfait);
-		
+
 		panelTableHorsForfait.setLayout(new BorderLayout());
 		panelTableHorsForfait.add(tableHorsForfait.getTableHeader(), BorderLayout.NORTH);
 		panelTableHorsForfait.add(tableHorsForfait, BorderLayout.CENTER);
-		
+
 		panelGlobalHorsForfait.setLayout(new BorderLayout());
 		panelGlobalHorsForfait.add(panelLabelHorsForfait, BorderLayout.NORTH);
 		panelGlobalHorsForfait.add(panelTableHorsForfait, BorderLayout.CENTER);
-		
+
 		// Instanciation de l'affichage des hors classification
 		panelLabelHorsClassification.setLayout(new FlowLayout());
 		panelLabelHorsClassification.add(libelleHorsClassification);
-		
+
 		panelTableHorsClassification.setLayout(new BorderLayout());
 		panelTableHorsClassification.add(tableHorsClassification.getTableHeader(), BorderLayout.NORTH);
 		panelTableHorsClassification.add(tableHorsClassification, BorderLayout.CENTER);
-		
+
 		panelGlobalHorsClassification.setLayout(new BorderLayout());
 		panelGlobalHorsClassification.add(panelLabelHorsClassification, BorderLayout.NORTH);
 		panelGlobalHorsClassification.add(panelTableHorsClassification, BorderLayout.CENTER);
 		panelGlobalHorsClassification.add(new JLabel(lesHorsClassification.get(0).getLibelleEtat()), BorderLayout.SOUTH);
-		
+
 		// Instanciation de l'affichage du bouton
 		panelBouton.setLayout(new FlowLayout());
 		panelBouton.add(validerFiche);
 		panelBouton.add(retour);
-				
+
 		panelGlobalFicheFrais.setLayout(new BorderLayout(20, 20));
 		panelGlobalFicheFrais.add(panelGlobalForfait, BorderLayout.NORTH);
 		panelGlobalFicheFrais.add(panelGlobalHorsForfait, BorderLayout.CENTER);
 		panelGlobalFicheFrais.add(panelGlobalHorsClassification, BorderLayout.SOUTH);
-		
+
 		panelGlobal.setLayout(new BorderLayout());
 		panelGlobal.add(panelGlobalFicheFrais, BorderLayout.NORTH);
 		panelGlobal.add(panelBouton, BorderLayout.CENTER);
-		
-		this.getContentPane().add(panelGlobal);
-		
-		this.setVisible(true); 
-	}
 
+		this.getContentPane().add(panelGlobal);
+
+		this.setVisible(true);
+	}
+	
+	
 	@Override
 	public void actionPerformed(ActionEvent event) {
 		if(event.getSource() == retour){
